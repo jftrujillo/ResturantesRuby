cd133f5af5b72df92c11be394b64367a9df55414
Jhon <jftrujillo@unicauca.edu.co>
2016-06-11 18:51:38 -0500

    add preferred sass syntax (file extension)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+
+Rails.configuration.sass.preferred_syntax = "sass"
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( main/styles/products.css  application-welcome.css application-session.css )
+Rails.application.config.assets.precompile += %w( application-welcome.css application-session.css )
+Rails.application.config.assets.precompile += %w( main/styles/products.css )
